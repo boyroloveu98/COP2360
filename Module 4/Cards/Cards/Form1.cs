03aacf6b887536575bde0c1297886d0d90ed17cc
Matt <mattcookemed@gmail.com>
2019-11-07 13:59:16 -0500

    hw module 4

--- Module 4/Cards/Cards/Form1.cs
+++ Module 4/Cards/Cards/Form1.cs
@@ -15,5 +15,68 @@ namespace Cards
         {
             InitializeComponent();
         }
+
+        //the showcard method accepts a string that names
+        //the selected card and displays the card
+        private void ShowCard(string card)
+        {
+            switch (card)
+            {
+                case "Ace of Spades":
+                    ShowAceSpades();
+                    break;
+                case "10 of Hearts":
+                    ShowTenHearts();
+                    break;
+                case "King of Clubs":
+                    ShowKingClubs();
+                    break;
+            }
+        }
+
+        // The showAceSpades method makes the ace of spades
+        // visible and the other cards invisible.
+        private void ShowAceSpades()
+        {
+            aceSpadesPictureBox.Visible = true;
+            tenHeartsPictureBox.Visible = false;
+            kingClubsPictureBox.Visible = false;
+        }
+
+        // The ShowTenHearts method makes the ten of hearts
+        // visible and the other cards invisible.
+        private void ShowTenHearts()
+        {
+            aceSpadesPictureBox.Visible = false;
+            tenHeartsPictureBox.Visible = true;
+            kingClubsPictureBox.Visible = false;
+        }
+
+        // The ShowKingClubs method makes the king of clubs
+        // visible and the other cards invisible.
+        private void ShowKingClubs()
+        {
+            aceSpadesPictureBox.Visible = false;
+            tenHeartsPictureBox.Visible = false;
+            kingClubsPictureBox.Visible = true;
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void showCardButton_Click(object sender, EventArgs e)
+        {
+            //if a card is selected in the list box, show the card to the user
+            if (cardListBox.SelectedIndex != -1)
+            {
+                ShowCard(cardListBox.SelectedItem.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Please select a card from the list box");
+            }
+        }
     }
 }
