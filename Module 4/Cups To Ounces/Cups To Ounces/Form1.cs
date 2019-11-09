4466190912af32f7225951c578ef80ddf10e9e98
Matt <mattcookemed@gmail.com>
2019-11-09 14:46:37 -0500

    prep assigments

---
+++ Module 4/Cups To Ounces/Cups To Ounces/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Cups_To_Ounces
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //create the CupsToOunces method that takes a number
+        // that number is the number of cups for the argument
+        // this will return the number of ounces in the cup value
+        private double CupsToOunces(double cups)
+        {
+            return cups * 8.0;
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            // create the variable that will hold the number of cups
+            double cups, ounces;
+
+            //get the number of cups from the user
+            if(double.TryParse(cupsTextBox.Text, out cups))
+            {
+                //conver the cups to ounces
+                ounces = CupsToOunces(cups);
+
+                //display the value
+                ouncesLabel.Text = ounces.ToString("n1");
+            }
+            else
+            {
+                //error mah dude
+                MessageBox.Show("Please enter a valid number.");
+            }
+        }
+    }
+}
---
+++ Module 4/North America/North America/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace North_America
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            ;
+            InitializeComponent();
+        }
+
+        // The get filename method gets a filename from the 
+        // user and assigns it to the variable passed as 
+        // an argument
+        private void GetFileName(out string selectedFile)
+        {
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                selectedFile = openFile.FileName;
+            }
+            else
+            {
+                selectedFile = "";
+            }
+        }
+
+        // the GetCountries method accepts a filename as an 
+        // argument. It opens the specified file and displays 
+        // its contents in the countriesListBox control.
+        private void GetCountries(string filename)
+        {
+            try
+            {
+                // Decclare a variable to hold a country name.
+                string countryName;
+
+                //declare a Streamreader variable
+                StreamReader inputFile;
+
+                // open the file and get a streamReader object
+                inputFile = File.OpenText(filename);
+
+                // clear out anything that is currently in the list box
+                countriesListBox.Items.Clear();
+
+                // read the contents of the file
+                while (!inputFile.EndOfStream)
+                {
+                    // Get the name of a country
+                    countryName = inputFile.ReadLine();
+
+                    // once it's been read ad the name of the country to the listbox
+                    countriesListBox.Items.Add(countryName);
+                }
+
+                //close the file
+                inputFile.Close();
+            } 
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void getCountriesButton_Click(object sender, EventArgs e)
+        {
+            string fileName;  // self explanatory
+
+            //get the filename from the user
+            GetFileName(out fileName);
+
+            //get the countries from that file
+            GetCountries(fileName);
+        }
+    }
+}
